f340e541206b34c0dc7be5c7a9601f3d83712b86
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-10-01 22:22:26 +0600

    Create 510A - Fox And Snake.cpp

---
+++ codeforces/510A - Fox And Snake.cpp
@@ -0,0 +1,34 @@
+//510A - Fox And Snake
+#include<iostream>
+using namespace std;
+
+int main() {
+	int m, n, count = 0;
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		if (i % 2 == 1) {
+			if (count % 2 != 0) {
+				cout << "#";
+				for (int i = 0; i < m - 1; i++) {
+					cout << ".";
+				}
+				cout << endl;
+			}
+			else {
+				for (int i = 0; i < m - 1; i++) {
+					cout << ".";
+				}
+				cout << "#" << endl;
+			}
+			count++;
+		}
+		else {
+			for (int i = 0; i < m; i++) {
+				cout << "#";
+			}
+			cout << endl;
+		}
+	}
+}
