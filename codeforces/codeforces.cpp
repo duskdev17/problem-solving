3406d7af2c447c928dec4a3288690cc5f1664a96
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-10-02 14:16:29 +0600

    Update codeforces.cpp

--- codeforces/codeforces.cpp
+++ codeforces/codeforces.cpp
@@ -1,34 +1,14 @@
-//510A - Fox And Snake
+//	118A - String Task
 #include<iostream>
 using namespace std;
 
 int main() {
-	int m, n, count = 0;
+	char str[100];
 
-	cin >> n >> m;
+	cin >> str;
+	int n = strlen(str);
 
 	for (int i = 0; i < n; i++) {
-		if (i % 2 == 1) {
-			if (count % 2 != 0) {
-				cout << "#";
-				for (int i = 0; i < m - 1; i++) {
-					cout << ".";
-				}
-				cout << endl;
-			}
-			else {
-				for (int i = 0; i < m - 1; i++) {
-					cout << ".";
-				}
-				cout << "#" << endl;
-			}
-			count++;
-		}
-		else {
-			for (int i = 0; i < m; i++) {
-				cout << "#";
-			}
-			cout << endl;
-		}
+		if(str[i] == )
 	}
-}	
+}
