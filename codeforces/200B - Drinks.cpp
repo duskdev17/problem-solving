bb6ae9ff17c6a51cb556bcc1affa3b9d71d4b220
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-02-06 20:13:51 +0600

    update

--- codeforces/200B - Drinks.cpp
+++ codeforces/200B - Drinks.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 int main(){
-    int n, a[100];
-
+    double n, percentage[100];
+	double temp; 
+	
     cin >> n;
 
-    for(int i=9; i<n; i++){
-        cin >> a[i];
+    for(int i=0; i<n; i++){
+      cin >> percentage[i];
     }
 
-    
+    for(int i=0; i<n; i++){
+      temp = temp + percentage[i]/n;
+    }
 
-}
+    cout << setprecision(12) << temp;
+
+  }
