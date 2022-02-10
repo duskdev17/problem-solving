30419d04c3db382e08a1934dc9a88d78e75a2143
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-02-10 05:01:37 -0800

    update

--- codeforces/1030A In Search of an Easy Problem.cpp
+++ codeforces/1030A In Search of an Easy Problem.cpp
@@ -2,26 +2,6 @@
 using namespace std;
 
 int main(){
-	int n, a[100], flag=0;
+	int n;
 	
-	cin >> n;
 	
-	for(int i=0; i<n; i++){
-		cin >> a[i];
-	}
-	
-	for(int i =0; i< n; i++){
-		if(a[i] == 0)
-			flag = 0;
-		else
-			flag = 1;
-	}
-	
-	if(flag = 1){
-		cout << "HARD" ;
-	}
-	else{
-		cout << "EASY";
-	}
-	
-}
