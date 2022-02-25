8d49e373ffeb23cdb1d751a994ee6aac431095ad
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-02-25 12:13:07 +0600

    update

--- codeforces/prime_numbers.cpp
+++ codeforces/prime_numbers.cpp
@@ -1,37 +1,20 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    int n; 
-    bool isPrime = false;
-
-    cout <<  "Enter a number: ";
-    cin >> n; 
-    
-    if(n==0 || n==1){
-        isPrime = false;
-    }
-    else{
-        for(int i=2; i<=n/2; i++){
-                if(n%i==0){
-                    isPrime = false;
-                    break;
-                }
-                else{
-                    isPrime = true;
-                    break;
-                }
-            }
-    }
-    
-
-    if(isPrime = false){
-        cout << n << " is not a prime number." << endl;
-    }
-    else{
-        cout << n << " is a prime number." << endl;
-    }
-
-    return 0;
-
-}
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+  int n, i, flag=0;  
+  cout << "Enter the Number to check Prime: ";  
+  cin >> n;    
+  for(i = 2; i <= n/2; i++)  
+  {  
+      if(n % i == 0)  
+      {  
+          cout<<"Number is not Prime."<<endl;  
+          flag=1;  
+          break;  
+      }  
+  }  
+  if (flag==0)  
+      cout << "Number is Prime."<<endl;  
+  return 0;  
+}  
