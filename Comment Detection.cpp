e00b1c7c9942804d0f22310983bb62b393e53012
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-04-08 21:22:16 +0600

    update

---
+++ Comment Detection.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string line;
+	
+	getline (cin, line);
+
+	if (line[0] == '/' && line[1] == '/'
+		&& line[2] != '/') {
+
+		cout << "It is a single-line comment";
+	}
+
+	else if (line[line.size() - 2] == '*'
+		&& line[line.size() - 1] == '/' && line[0] == '/' && line[1] == '*') {
+
+		cout << "It is a multi-line comment";
+	}
+
+	else{
+		cout << "It is not a comment";
+	}
+
+	
+	
+	return 0;
+}
+
