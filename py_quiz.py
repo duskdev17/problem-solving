fcd45e209b7f444cb23d2c7dff7af0f4e0e9c52b
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-04-19 21:29:20 +0600

    Create py_quiz.py

---
+++ py_quiz.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+python quiz L-3 T-1
+"""
+
+x = 2
+y = 3
+
+print(x**y)
+
+print(3*1**2)
+
+a = ["A", "B", "C"]
+a.clear()
+print(a)
+
+
+L = [1, 23, 'hello', 1]
+print(type(L))
+
+"""
+x = ['ab', 'cd']
+for i in x:
+    x.append(i.upper())
+    print(x)
+    """
+    
+x = 'abcd'
+for i in range(len(x)):
+    x = 'a'
+    print(x)
