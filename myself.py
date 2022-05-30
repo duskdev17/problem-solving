7ab2f38db4e3c71aced63cdbdc5218853859046d
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-05-31 00:19:22 +0600

    Create myself.py

---
+++ myself.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 31 00:05:12 2022
+
+@author: Adrik
+"""
+
+
+name = input("Enter your name: ")
+id_no = input("Enter your ID: ")
+age = input("Enter your age: ")
+location = input("Enter address: ")
+
+
+print("Hi, I am", name, ". I am", age, "years old. ", "I am from", location, ". My ID is", id_no, ".")
